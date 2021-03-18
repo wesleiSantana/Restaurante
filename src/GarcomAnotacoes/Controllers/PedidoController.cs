6c31a56784d9be947c0978fa039ed75db0195451
wesleiSantana <wesleymoreira2006@gmail.com>
2021-03-18 17:35:19 -0300

    Realizando a criação do sistema

---
+++ src/GarcomAnotacoes/Controllers/PedidoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GarcomAnotacoes.Controllers
+{
+    public class PedidoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/GarcomAnotacoes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GarcomAnotacoes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
